10860ec37a3b2c40c8d8d22b1f9b3c37f4b0c3fe
Deltaes <swhong@deltaes.co.kr>
2024-12-04 17:58:37 +0900

    Design: 메인페이지 작업 중
    이미지 슬라이드 파일 변환 중
    나머지 부분 제작 및 틀 완료
    고객사 부분 이미지 처리 고민

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,113 @@
+import Link from "next/link";
 import styles from "../app/styles/page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <section className={styles.section}>
-        <div>
+        <div className={styles.imgSlice}>
           <p>슬라이드 이미지</p>
         </div>
-        <div className={styles.article}>
-          <article>제품안내</article>
-          <article>적용산업</article>
-          <article>엔지니어링 컨설팅 서비스</article>
-          <article>교육안내</article>
-        </div>
+        <article className={styles.article}>
+          <div className={styles.f}>
+            <Link href="">
+              <h2>제품안내</h2>
+              <p>
+                델타이에스가 제공하는 <br /> CAE 프로그램을 확인 하세요
+              </p>
+              <span>+</span>
+            </Link>
+          </div>
+          <div className={styles.f}>
+            <Link href="">
+              <h2>적용산업</h2>
+              <p>
+                우리의 일상 전반에 적용된 <br /> 엔지니어링 사례를 확인 하세요
+              </p>
+              <span>+</span>
+            </Link>
+          </div>
+          <div className={styles.f}>
+            <Link href="">
+              <h2>엔지니어링 컨설팅 서비스</h2>
+              <p>
+                우리의 일상 전반에 적용된 <br /> 엔지니어링 사례를 확인 하세요
+              </p>
+              <span>+</span>
+            </Link>
+          </div>
+          <div className={styles.f}>
+            <Link href="">
+              <h2>교육안내</h2>
+              <p>
+                고객이 원하는 눈높이에 <br /> 맞춘 교육을 지원 합니다
+              </p>
+              <span>+</span>
+            </Link>
+          </div>
+        </article>
       </section>
       <section className={styles.section1}>
-        <div className={styles.article1}>
-          <article>
-            <h3>교육 일정표</h3>
-            <div></div>
-            <button>더보기</button>
-          </article>
-          <article>
-            <h3>행사소식</h3>
-            <div></div>
-            <button>더보기</button>
-          </article>
-          <article>
-            <div className={styles.article1}>
-              <button>공지사항</button>
-              <button>제품소식</button>
-              <button>자료실</button>
+        <article className={styles.article1}>
+          <div className={styles.s}>
+            <div className={styles.sHeader}>
+              <h3>교육 일정표</h3>
             </div>
-          </article>
-        </div>
+            <div className={styles.sContainer}>
+              <div className={styles.sContent}>
+                <div className={styles.sContentSub}>sodyd</div>
+                <div className={styles.sContentSub}>sodyd</div>
+              </div>
+              <div className={styles.sFooter}>
+                <button className={styles.sButton}>더보기</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.s}>
+            <div className={styles.sHeader}>
+              <h3>행사소식</h3>
+            </div>
+            <div className={styles.sContainer2}>
+              <div className={styles.sContent}>
+                <div className={styles.sContentSub2}>sodyd</div>
+                <div className={styles.sContentSub2}>sodyd</div>
+              </div>
+              <div className={styles.sFooter}>
+                <button className={styles.sButton}>더보기</button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.s}>
+              <div className={styles.sHeader2}>
+                <div className={styles.sHeader2Sub}>
+                  <h3>공지사항</h3>
+                </div>
+                <div className={styles.sHeader2Sub}>
+                  <h3>제품소식</h3>
+                </div>
+                <div className={styles.sHeader2Sub}>
+                  <h3>자료실</h3>
+                </div>
+              </div>
+              <div className={styles.sContainer2}>
+                <div className={styles.sContent}>
+                  <div className={styles.sContentSub3}>sodyd</div>
+                </div>
+                <div className={styles.sFooter}>
+                  <button className={styles.sButton}>더보기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
       </section>
       <section className={styles.section2}>
-        <article>
-          <div>
+        <article className={styles.article2}>
+          <p>
             델타이에스 <br />
             고객사
-          </div>
+          </p>
           <div>슬라이드로 제작</div>
         </article>
       </section>
