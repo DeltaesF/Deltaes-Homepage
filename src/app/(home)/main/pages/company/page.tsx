682da3213b5da25b5455fb0801961e88474ecc00
Deltaes <swhong@deltaes.co.kr>
2025-01-22 10:03:53 +0900

    Feat: api를 이용해 클라이언트에 이미지 보여주는 custom hook 제작
    api를 이용해서 클라이언트에 이미지를 보여주는 custom hook 제작 및 주석처리

--- src/app/(home)/main/pages/company/page.tsx
+++ src/app/(home)/main/pages/company/page.tsx
@@ -3,34 +3,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import useFetchImages from "@/app/hooks/useFetchImages";
 
 export default function Company() {
-  const [imageSrc, setImageSrc] = useState<string[] | null[]>([null]);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function fetchImage() {
-      try {
-        const imageNames = ["adt1.jpg", "adt2.jpg", "adt3.jpg"];
-
-        // 각 이미지를 fetch하고 Blob URL로 변환
-        const imagePromises = imageNames.map(async (imageName) => {
-          const res = await fetch(`/api/getftp/${imageName}`);
-          if (!res.ok) throw new Error(`Failed to load ${imageName}`);
-          const imageBlob = await res.blob(); // 응답에서 Blob 형태로 이미지를 받아옵니다.
-          return URL.createObjectURL(imageBlob); // 상태를 업데이트하여 이미지 URL을 설정합니다.
-        });
-
-        // 모든 이미지 요청을 동시에 처리
-        const imageUrls = await Promise.all(imagePromises);
-        setImageSrc(imageUrls); // 모든 이미지를 상태에 설정
-      } catch (err) {
-        setError(err instanceof Error ? err.message : "An error occurred");
-      }
-    }
-
-    fetchImage();
-  }, []);
+  const { imageSrc, error } = useFetchImages([
+    "adt1.jpg",
+    "adt2.jpg",
+    "adt3.jpg",
+  ]);
 
   if (error) {
     return <p>Error: {error}</p>;
---
+++ src/app/hooks/useFetchImages.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+// FTP에서 이미지를 불러와서 URL로 반환하는 custom hook
+export default function useFetchImages(imageNames: string[]) {
+  // imageNames는 배열에 있는 각 이미지의 URL을 저장하는 상태 변수
+  const [imageSrc, setImageSrc] = useState<string[]>(
+    new Array(imageNames.length).fill(null), // 초기 값 모든 항목을 null로 설정
+  );
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    // imageNames 배열에 변화가 있을 때마다 이미지를 다시 fetch하도록 useEffect 사용
+    async function fetchImages() {
+      try {
+        // imageNames 배열을 순회하며 각 이미지 파일 fetch 후 blob URL로 반환
+        const imagePromises = imageNames.map(async (imageName, index) => {
+          const res = await fetch(`/api/getftp/${imageName}`);
+          if (!res.ok) throw new Error(`failed to load ${imageName}`);
+          const imageBlob = await res.blob(); // blob 형태로 이미지 데이터 받음
+          return URL.createObjectURL(imageBlob); // blob 데이터 URL로 변환하여 반환
+        });
+
+        const imageUrls = await Promise.all(imagePromises); // Promise.all을 사용해서 모든 이미지 반환
+        setImageSrc(imageUrls);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "An error occurred");
+      }
+    }
+    fetchImages();
+  }, [imageNames]); // imageNames가 변경될 때마다 useEffect가 재실행됨
+
+  return { imageSrc, error }; // 훅의 반환값: 이미지 URL 배열과 에러 메시지를 객체 형태로 반환
+}
