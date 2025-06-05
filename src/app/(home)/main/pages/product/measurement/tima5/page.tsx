a74a04e9c5bf7d92965bb96418dc4a0634262d89
Deltaes <swhong@deltaes.co.kr>
2025-06-05 11:06:47 +0900

    Feat: Tima5 코드 수정 완료

--- src/app/(home)/main/pages/product/measurement/tima5/page.tsx
+++ src/app/(home)/main/pages/product/measurement/tima5/page.tsx
@@ -10,9 +10,8 @@ import useFetchImages from "@/app/hooks/useFetchImages";
 export default function Tima5() {
   const { imageSrc, loading, error } = useFetchImages([
     "tima5_1.avif",
-    "tima5_2.avif",
-    "tima5_3.avif",
-    "tima5_4.avif",
+    "tima5_2.png",
+    "tima5_3.png",
     "tima5_5.avif",
     "tima5_6.avif",
     "tima5_7.avif",
@@ -20,8 +19,10 @@ export default function Tima5() {
     "tima5_9.avif",
     "tima5_10.avif",
     "timapulse2.avif",
-    "tima5brochure.pdf",
-    "tima5data.pdf",
+    "tima5_11.jpg",
+    "tima5_12.png",
+    "tima5_13.jpg",
+    "tima5_14.jpg",
   ]);
 
   if (error) {
@@ -70,13 +71,10 @@ export default function Tima5() {
                     NANOTEST TIMA 5는 열 인터페이스 재료 및 낮은 범위에서 중간
                     범위의 열 전도성을 갖는 기타 재료의 열 특성을 측정하기 위한
                     ASTM D 5470을 완벽하게 준수하는 측정 시스템입니다.
-                    <br />
-                    유리 칩은 일회용이지만 세척하여 재사용하는 것이 좋습니다.
-                    칩홀더는 컴팩트하며 카드키처럼 랙에 삽입됩니다. 더 높은
-                    온도에서의 측정을 위해 칩에는 반대쪽에 두 개의 가열 요소가
-                    포함되어 있어 최대 180°C까지 가열할 수 있습니다.
-                    <br />열 인터페이스 재료의 분석이 간단하고, 포괄적인 TIMA
-                    5는 높은 수준의 기능성과 탁월한 사용 편의성을 제공 합니다.
+                  </p>
+                  <p>
+                    열 인터페이스 재료의 분석이 간단하고, 포괄적인 TIMA 5는 높은
+                    수준의 기능성과 탁월한 사용 편의성을 제공 합니다.
                   </p>
                   <button>구매 문의</button>
                 </div>
@@ -84,9 +82,9 @@ export default function Tima5() {
             </div>
           </div>
           <div className={styles.secondWrapper}>
-            {imageSrc[10] && (
+            {imageSrc[9] && (
               <Image
-                src={imageSrc[10]}
+                src={imageSrc[9]}
                 alt="TIMA 배경 이미지"
                 fill
                 sizes="100vw"
@@ -120,6 +118,45 @@ export default function Tima5() {
           </div>
           <div className={styles.thirdWrapper}>
             <div className={styles.thirdContent}>
+              <div className={styles.thirdPoint}>
+                <ul>
+                  <h2>주요 특징</h2>
+                  <li>» ASTM D5470 완전 준수</li>
+                  <li>» 컴팩트 및 올인원 디자인</li>
+                  <li>» 자동화 및 예약 테스트 기능</li>
+                  <li>» 신속 교체 가능한 테스트 헤드</li>
+                  <li>» 고정밀 두께 모니터링</li>
+                  <li>» 사용자 경험 최적화 및 간편한 사용성</li>
+                </ul>
+                <ul>
+                  <h3>주요 소재 및 컴파운드 물성치</h3>
+                  <li>» 열저항</li>
+                  <li>» 체적 열전도율</li>
+                  <li>» 접촉 저항</li>
+                </ul>
+
+                <ul>
+                  <h3>Key testing schemes</h3>
+                  <li>» 온도 의존성</li>
+                  <li>» 압력 의존성</li>
+                  <li>» 열적 성능</li>
+                  <li>» 인터페이스 품질</li>
+                  <li>» 열기계적 안정성</li>
+                  <li>» 노화 거동 / 수명 예측</li>
+                </ul>
+
+                <ul>
+                  <h3>Scope of samples</h3>
+                  <li>» TIM 소재</li>
+                  <li>» 페이스트 및 그리스</li>
+                  <li>» 갭 패드 및 갭 필러</li>
+                  <li>» 포일 및 시트</li>
+                  <li>» 접착제 및 경화 소재</li>
+                  <li>» 몰드 컴파운드</li>
+                  <li>» 언더필러</li>
+                  <li>» 기판 및 인터포저</li>
+                </ul>
+              </div>
               <h2>Technical Specification</h2>
               <div className={styles.thirdImgP}>
                 <div className={styles.thirdImg1_1}>
@@ -133,7 +170,7 @@ export default function Tima5() {
                   )}
                 </div>
               </div>
-              <h2>Sample properties</h2>
+              <h2>Sample properties & Measurement conditions</h2>
               <div className={styles.thirdImgP}>
                 <div className={styles.thirdImg1_2}>
                   {imageSrc[2] && (
@@ -146,9 +183,9 @@ export default function Tima5() {
                   )}
                 </div>
               </div>
-              <h2>Measurement conditions</h2>
+              <h2>Measurement accuracy</h2>
               <div className={styles.thirdImgP}>
-                <div className={styles.thirdImg1_3}>
+                <div className={styles.thirdImg1_4}>
                   {imageSrc[3] && (
                     <Image
                       src={imageSrc[3]}
@@ -159,9 +196,9 @@ export default function Tima5() {
                   )}
                 </div>
               </div>
-              <h2>Measurement accuracy</h2>
+              <h2>Software screenshots</h2>
               <div className={styles.thirdImgP}>
-                <div className={styles.thirdImg1_4}>
+                <div className={styles.thirdImg1_5}>
                   {imageSrc[4] && (
                     <Image
                       src={imageSrc[4]}
@@ -172,172 +209,143 @@ export default function Tima5() {
                   )}
                 </div>
               </div>
-              <h2>Software screenshots</h2>
-              <div className={styles.thirdImgP}>
-                <div className={styles.thirdImg1_5}>
-                  {imageSrc[5] && (
-                    <Image
-                      src={imageSrc[5]}
-                      alt="전자기기 및 반도체"
-                      fill
-                      style={{ objectFit: "cover" }}
-                    />
-                  )}
-                </div>
+              <div className={styles.thirdPoint2_1}>
+                <h3>단순하면서도 다재 다능한 솔루션</h3>
+                <p>
+                  TIMA5는 실험실 및 산업용 열 측정을 위한 종합적인 장비로, ASTM
+                  D5470 규정에 따라 다양한 열 특성 분석을 수행할 수 있도록
+                  설계되었습니다.
+                </p>
               </div>
-              <h2>온칩 가열</h2>
-              <div className={styles.thirdImgP}>
-                <div className={styles.thirdDes2}>
-                  <p>
-                    열 인터페이스 재료 분석기(TIMA 5)는 열 인터페이스 재료뿐만
-                    아니라 낮거나 중간 정도의 열 전도율을 갖는 기타 재료 등급의
-                    열 및 열-기계적 특성을 분석하기 위한 범용 시스템입니다.
-                    <br /> ASTM D5470는 업계 전반의 일반적으로 사용되는 TIM
-                    특성화 표준으로, 대부분의 애플리케이션 시나리오에 적응하는
-                    데 필요한 가변성을 제공합니다.
-                  </p>
-                  <p>
-                    실험실에 적합하며 컴팩트하고 모든 냉수 공급이 제공됩니다.
-                    하루 교육만으로 장치를 완전히 이해하고 모든 유형의 특성화 및
-                    재료 연구를 용이하게 수행할 수 있습니다.
-                  </p>
-                </div>
-                <div className={styles.thirdImg}>
-                  {imageSrc[6] && (
-                    <Image
-                      src={imageSrc[6]}
-                      alt="전자기기 및 반도체"
-                      fill
-                      style={{ objectFit: "cover" }}
-                    />
-                  )}
-                </div>
+              <div className={styles.thirdPoint2}>
+                <ul>
+                  <h2>샘플 범위</h2>
+                  <li>◎ Greases and pastes</li>
+                  <li>◎ Cured gap fillers and adhesives</li>
+                  <li>◎ Anisotropic composites</li>
+                  <li>◎ Phase change materials</li>
+                </ul>
+                <ul>
+                  <h3>출력 데이터</h3>
+                  <li>◎ Overall thermal resistance</li>
+                  <li>◎ Effective and bulk thermal conductivity</li>
+                  <li>◎ Thermal interface resistance</li>
+                  <li>◎ Pressure and temperature dependency</li>
+                </ul>
+                <ul>
+                  <h3>고급 응용 분야</h3>
+                  <li>◎ Curing parameters study</li>
+                  <li>◎ Boundary conditions study</li>
+                  <li>◎ In-situ reliability investigation</li>
+                  <li>◎ Extreme conditions testing</li>
+                </ul>
               </div>
-              <h2>우수한 TIM 특성화</h2>
-              <div className={styles.thirdImgP}>
-                <div className={styles.thirdImg2}>
-                  {imageSrc[7] && (
-                    <Image
-                      src={imageSrc[7]}
-                      alt="전자기기 및 반도체"
-                      fill
-                      style={{ objectFit: "cover" }}
-                    />
-                  )}
-                </div>
-                <div className={styles.thirdDes2}>
+              <div className={styles.thirdPoint2Img1}>
+                {imageSrc[10] && (
+                  <Image
+                    src={imageSrc[10]}
+                    alt="전자기기 및 반도체"
+                    fill
+                    style={{ objectFit: "contain" }}
+                  />
+                )}
+              </div>
+              <div className={styles.thirdPointDeC}>
+                <div className={styles.thirdPointDe}>
+                  <h2>ASTM D 5470 표준을 넘어선 혁신적인 성능</h2>
                   <p>
-                    ASTM 표준 D5470은 열 재료를 특성화하고 다음을 얻는 절차를
-                    설명합니다.벌크 열전도율그리고인터페이스 저항. 첫 번째 값은
-                    재료별로 다르지만 두 번째 값은 상황에 가장 민감하며 현대
-                    응용 분야에서 그 중요성이 점점 더 커지고 있습니다. 특히 TIM
-                    특성화의 경우 ASTM D5470은 기존의 다른 방법보다 우수합니다.
-                    일반적으로 인터페이스 저항은 다음 매개변수에 따라 달라지며,
-                    이 매개변수는 모두 TIMA에서 구성 가능한 옵션입니다.
+                    TIMA 5는 ASTM 표준 D5470-17에서 규정된 시험 방법을 완벽히
+                    충족하는 동시에, 완전 자동화된 특성 평가 및 선택적 TTV
+                    모듈을 추가하여 ASTM에 포함되지 않는 여러 Test를 제공합니다.
                   </p>
                   <ul>
-                    <li>TIM 및 그 구성(매트릭스, 입자 등)</li>
-                    <li>표면 재질, 품질 및 거칠기</li>
-                    <li>접촉력 및 접착력</li>
-                    <li>표면 온도</li>
+                    <li>◎ Interchangeable test heads</li>
+                    <li>◎ High-precision curing tool</li>
+                    <li>◎ Optional TTV module</li>
+                    <li>◎ Fully automated measurement</li>
+                    <li>◎ Up to 150°C sample temperature</li>
+                    <li>
+                      ◎ ± 300 N clamping and tensile force with integrated load
+                      cell
+                    </li>
                   </ul>
                 </div>
-              </div>
-              <h2>원동력</h2>
-              <div className={styles.thirdImgP}>
-                <div className={styles.thirdDes2}>
-                  <p>
-                    강력한 모터를 사용하면 최대 300N의 수직력으로 시료 재료를
-                    압축하고 인장할 수 있습니다. 이는 샘플 단면에 따라 최대
-                    3MPa/430PSI로 변환됩니다. 이를 통해 TIM 응용 분야의 상당한
-                    부담을 해결하고 재료 개발을 위한 포괄적인 데이터를 얻고
-                    데이터시트에 제공할 수 있습니다.
-                  </p>
-                  <p>
-                    샘플에 가해지는 힘을 제어하는 ​​동안 TIMA는 몇 미크론
-                    분해능으로 샘플 두께를 정밀하게 모니터링하여 재료 압축에
-                    대한 정확한 정보를 제공하고 샘플의 유효 열전도도를
-                    모니터링합니다.
-                  </p>
+                <div className={styles.thirdPointDeImg}>
+                  <div className={styles.thirdPoint2Img1_2}>
+                    {imageSrc[11] && (
+                      <Image
+                        src={imageSrc[11]}
+                        alt="전자기기 및 반도체"
+                        fill
+                        style={{ objectFit: "contain" }}
+                      />
+                    )}
+                  </div>
+
+                  <div className={styles.thirdPointDeImgBottom}>
+                    <div className={styles.thirdImg2_1}>
+                      {imageSrc[6] && (
+                        <Image
+                          src={imageSrc[6]}
+                          alt="전자기기 및 반도체"
+                          fill
+                          style={{ objectFit: "cover" }}
+                        />
+                      )}
+                    </div>
+                    <div className={styles.thirdImg3_1}>
+                      {imageSrc[7] && (
+                        <Image
+                          src={imageSrc[7]}
+                          alt="전자기기 및 반도체"
+                          fill
+                          style={{ objectFit: "cover" }}
+                        />
+                      )}
+                    </div>
+                  </div>
                 </div>
-                <div className={styles.thirdImg3}>
-                  {imageSrc[8] && (
+              </div>
+              <div className={styles.thirdPoint2_1}>
+                <h3>열화 및 신뢰성 평가</h3>
+                <p>
+                  TIMA는 열 기계적 스트레스에 노출된 열 인터페이스 소재의 가속
+                  수명 테스트를 수행할 수 있습니다. 실제 적용 환경에서 발생하는
+                  기계적 변형을 정밀하게 재현함으로써, 소재의 신뢰성과 장기
+                  안정성을 평가할 수 있습니다.
+                </p>
+              </div>
+              <div className={styles.thirdPointDeImg2}>
+                <div className={styles.thirdPoint2Img1_3}>
+                  {imageSrc[12] && (
                     <Image
-                      src={imageSrc[8]}
+                      src={imageSrc[12]}
                       alt="전자기기 및 반도체"
                       fill
-                      style={{ objectFit: "cover" }}
+                      style={{ objectFit: "contain" }}
+                    />
+                  )}
+                </div>
+                <div className={styles.thirdPoint2Img1_4}>
+                  {imageSrc[13] && (
+                    <Image
+                      src={imageSrc[13]}
+                      alt="전자기기 및 반도체"
+                      fill
+                      style={{ objectFit: "contain" }}
                     />
                   )}
                 </div>
               </div>
-              <h2>자동화되고 표준화된 측정</h2>
-              <div className={styles.thirdImgP}>
-                <div className={styles.thirdImg2}>
-                  {imageSrc[9] && (
-                    <Image
-                      src={imageSrc[9]}
-                      alt="전자기기 및 반도체"
-                      fill
-                      style={{ objectFit: "cover" }}
-                    />
-                  )}
-                </div>
-                <div className={styles.thirdDes2}>
-                  <p>
-                    TIMA 측정 시스템은 완전 자동화되어 있어서 재료를 넣고 측정을
-                    생산성을 높일 수 있습니다. ASTM D 5470 표준에 따른 다양한
-                    두께의 내구성 테스트와 페이스트나 갭 필러의 주기적인 검사를
-                    포함합니다. 모든 측정 데이터는 현장에서 바로 모니터링되고
-                    저장되어, 관련 소프트웨어를 통해 쉽게 확인이 가능합니다.
-                    이를 통해 측정이나 샘플의 노화 정도를 적시에 확인하고,
-                    적절한 시점에 다음 단계로 넘어가기 위한 결정을 할 수
-                    있습니다. 이러한 과정은 시간을 절약하고 개발 속도를
-                    가속화하는 데 도움을 줍니다. TIMA 소프트웨어는 회사의 표준
-                    작업 절차를 마련하고, 측정의 반복성과 비교 가능성을 높이는
-                    데에도 유용합니다.
-                  </p>
-                  <div className={styles.thirdDes2_1}>
-                    <div>
-                      <h3>주요 특징</h3>
-                      <ul>
-                        <li>TIM 및 그 구성(매트릭스, 입자 등)</li>
-                        <li>표면 재질, 품질 및 거칠기</li>
-                        <li>접촉력 및 접착력</li>
-                        <li>표면 온도</li>
-                      </ul>
-                    </div>
-                    <div>
-                      <h3>주요 출력 재료 및 복합 특성</h3>
-                      <ul>
-                        <li>TIM 및 그 구성(매트릭스, 입자 등)</li>
-                        <li>표면 재질, 품질 및 거칠기</li>
-                        <li>접촉력 및 접착력</li>
-                        <li>표면 온도</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className={styles.thirdDes2_1}>
-                    <div>
-                      <h3>주요 테스트 계획</h3>
-                      <ul>
-                        <li>TIM 및 그 구성(매트릭스, 입자 등)</li>
-                        <li>표면 재질, 품질 및 거칠기</li>
-                        <li>접촉력 및 접착력</li>
-                        <li>표면 온도</li>
-                      </ul>
-                    </div>
-                    <div>
-                      <h3>샘플 범위</h3>
-                      <ul>
-                        <li>TIM 및 그 구성(매트릭스, 입자 등)</li>
-                        <li>표면 재질, 품질 및 거칠기</li>
-                        <li>접촉력 및 접착력</li>
-                        <li>표면 온도</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
+              <div className={styles.thirdPoint}>
+                <ul>
+                  <li>◎ In-situ monitoring of aging / degradation</li>
+                  <li>◎ Highly accelerated: 5000 cycles per day</li>
+                </ul>
+                <ul>
+                  <li>◎ Application-related testing conditions</li>
+                  <li>◎ Thickness- and pressure-controlled cycling</li>
+                </ul>
               </div>
             </div>
           </div>
