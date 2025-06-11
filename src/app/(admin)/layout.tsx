465531477e5ba4d0a1f53e725501f91b3d7c07c6
Deltaes <swhong@deltaes.co.kr>
2025-06-11 14:32:32 +0900

    Feat: 주소 입력시 프로필 페이지 접속 차단 코드 추가

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,10 +1,28 @@
 // 관리자 페이지 새창으로 보여주기 위해 제작
 
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useUser } from "../context/UserContext";
+import { useEffect } from "react";
+
 export default function LoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user && user.role !== "admin") {
+      router.replace("/main");
+    }
+  }, [user]);
+
+  if (!user) return <div>로딩 중...</div>;
+  if (user.role !== "admin") return null;
+
   return (
     <html lang="en">
       <body className="login-layout antialiased" style={{ all: "unset" }}>
