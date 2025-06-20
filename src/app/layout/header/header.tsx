578ee0c63113ca25e1c0c1c00150417112bcda17
Deltaes <swhong@deltaes.co.kr>
2025-06-20 11:25:53 +0900

    Style: css 수정 완료

--- src/app/layout/header/header.tsx
+++ src/app/layout/header/header.tsx
@@ -140,7 +140,7 @@ export default function Header() {
                 <Link href="/main/pages/product">제품안내</Link>
                 <ul className={styles.dropdownMenu}>
                   <li className={`${styles.li} ${styles.subDropdown}`}>
-                    시뮬레이션 소프트웨어
+                    <Link href=""> 시뮬레이션 소프트웨어</Link>
                     <ul className={styles.subMenu}>
                       <li>
                         <Link href="/main/pages/product/simulation/flothermxt">
@@ -190,7 +190,7 @@ export default function Header() {
                     </ul>
                   </li>
                   <li className={`${styles.li} ${styles.subDropdown}`}>
-                    측정장비
+                    <Link href="">측정장비</Link>
                     <ul className={styles.subMenu}>
                       <li>
                         <Link href="/main/pages/product/measurement/powertester">
@@ -225,7 +225,7 @@ export default function Header() {
                     </ul>
                   </li>
                   <li className={`${styles.li} ${styles.subDropdown}`}>
-                    설계 소프트웨어
+                    <Link href="">설계 소프트웨어</Link>
                     <ul className={styles.subMenu}>
                       <li>
                         <Link href="/main/pages/product/design/adt">
